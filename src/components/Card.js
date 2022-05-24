7854452c394693eba539876a4d8b462b8f2b913f
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-05-24 21:58:07 +0530

    card css DOne

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,13 +7,10 @@ const Card = ({ student, count }) => {
       <Link to={"detail/" + student.roll} style={{ textDecoration: "none", color: "black", cursor: "pointer" }}>
         <div className="clg-rank">#_{count}</div>
         <div className="name">{student.name}</div>
-        <div className="roll-no">
-          {student.roll} <span className="branch">{student.branch}</span>
-        </div>
-        <div className="cgpi">
-          <span className="sgpi">{student.sgpi}</span>
-          {student.cgpi}
-        </div>
+        <div className="roll-no">{student.roll} </div>
+        <div className="branch">{student.branch}</div>
+        <div className="cgpi">CGPI: {student.cgpi}</div>
+        <div className="sgpi">SGPI: {student.sgpi}</div>
       </Link>
     </div>
   );
